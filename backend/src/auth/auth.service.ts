40ba745eb64d1796d4800c0ecd45557d440caf23
Facundo Gayoso <facusbrr@gmail.com>
2025-06-17 10:46:31 -0300

    refactor(auth): update controller and service, remove tests

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { RegisterDto } from './dtos/register.dto';
+import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dtos/login.dto';
+
+// Simulación de user
+const users = new Map<string, { email: string; password: string }>();
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async register(dto: RegisterDto) {
+    const { email, password } = dto;
+
+    if (users.has(email)) {
+      throw new Error(`El ${email} está en uso.`);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    users.set(email, { email, password: hashedPassword });
+
+    return { message: 'Usuario registrado correctamente' };
+  }
+
+  async login(dto: LoginDto) {
+    const { email, password } = dto;
+
+    const user = users.get(email);
+    if (!user) {
+      throw new UnauthorizedException('Credenciales Inválidas');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Credenciales Inválidas');
+    }
+
+    // Aca va el token JWT
+    return { message: 'Inicio de sesión correctamente' };
+  }
+}
---
+++ backend/src/auth/dtos/register.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+  // @IsString() name: string;
+}
