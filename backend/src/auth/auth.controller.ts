60d422af781322af16df664de897fd27bd0c879d
Facundo Gayoso <facusbrr@gmail.com>
2025-06-17 13:23:57 -0300

    chore(auth): remove incorrect import

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,7 +2,6 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dtos/register.dto';
 import { LoginDto } from './dtos/login.dto';
-import { Throttle } from '@nestjs/throttler';
 
 @Controller('auth')
 export class AuthController {
