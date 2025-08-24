0a5acf70ea2ae737bfb117b1aaf94a685b6827a0
Facundo Gayoso <facusbrr@gmail.com>
2025-08-24 16:20:29 -0300

    feat(auth): create DTO and validations for user registration

--- backend/src/auth/dtos/register.dto.ts
+++ backend/src/auth/dtos/register.dto.ts
@@ -1,27 +1,41 @@
 import {
   IsEmail,
   IsEnum,
-  IsOptional,
+  IsNotEmpty,
   IsString,
+  Matches,
   MinLength,
 } from 'class-validator';
-import { Role } from '../interfaces/role.enum';
+import { RoleName } from '@prisma/client';
 
+/**
+ * RegisterDto define la estructura y las reglas de validación para los datos
+ * que se reciben en el endpoint de registro de nuevos usuarios.
+ * NestJS usará esta clase para validar automáticamente el cuerpo de la petición.
+ */
 export class RegisterDto {
   @IsEmail({}, { message: 'El correo electrónico no es válido' })
+  @IsNotEmpty()
   email: string;
 
   @IsString()
   @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+    message:
+      'La contraseña debe contener al menos una mayúscula, una minúscula y un número',
+  })
+  @IsNotEmpty()
   password: string;
 
   @IsString()
+  @IsNotEmpty()
   firstName: string;
 
   @IsString()
+  @IsNotEmpty()
   lastName: string;
 
-  @IsEnum(Role, { message: 'Rol inválido' })
-  @IsOptional()
-  role?: Role; // Por defecto será STUDENT
+  @IsEnum(RoleName)
+  @IsNotEmpty()
+  role: RoleName;
 }
