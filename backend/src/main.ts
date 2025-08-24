c45311ecaa3e92f34d567c07aacb5aca2899ff42
Facundo Gayoso <facusbrr@gmail.com>
2025-08-24 16:59:50 -0300

    chore(app): configure swagger and global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ThrottlerExceptionFilter } from './shared/filters/throttler-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Conciencia API')
+    .setDescription('Documentación de la API para el proyecto Conciencia')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalFilters(new ThrottlerExceptionFilter());
 
